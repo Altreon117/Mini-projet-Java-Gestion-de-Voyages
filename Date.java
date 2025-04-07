cd4e97c3628fca1f43ef47646e44fc4551cbcf77
Tyrullr <68707110+Tyrullr@users.noreply.github.com>
2025-04-07 16:28:06 +0200

    ajout de 2 fichier

--- Date.java
+++ Date.java
@@ -1,2 +1,39 @@
-public class Date {
+public class date {
+    private int jours;
+    private  int  mois;
+    private int annee;
+
+    public date(int jours, int mois, int annee) {
+        this.jours = jours;
+        this.mois = mois;
+        this.annee = annee;
+    }
+
+    public int getJours() {
+        return jours;
+    }
+    public void setJours(int jours) {
+        this.jours = jours;
+    }
+    public int getMois() {
+        return mois;
+    }
+    public void setMois(int mois) {
+        this.mois = mois;
+    }
+    public int getAnnee() {
+        return annee;
+    }
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    @Override
+    public String toString() {
+        return "date{" +
+                "jours=" + jours +
+                ", mois=" + mois +
+                ", annee=" + annee +
+                '}';
+    }
 }
--- PlaneTicket.java
+++ PlaneTicket.java
@@ -1,2 +1,17 @@
 public class PlaneTicket {
+    private String Reference;
+    public PlaneTicket(String Reference) {
+        this.Reference = Reference;
+    }
+
+    public String getReference() {
+        return Reference;
+    }
+    public void setReference(String reference) {
+        Reference = reference;
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
